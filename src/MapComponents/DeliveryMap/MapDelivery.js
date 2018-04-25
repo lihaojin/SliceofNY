202a113c7ab75dea442c0852bd0f19556c70f9c2
William Newman <william.newman94@gmail.com>
2018-04-24 22:09:37 -0400

    got rid of unnecessary classes

--- src/MapComponents/DeliveryMap/MapDelivery.js
+++ src/MapComponents/DeliveryMap/MapDelivery.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import MapContainer from './MapContainer';
+import pizzaBox from './images/pizza-box-clipart-1.png'
+import bike from './images/bike.jpg'
+import Geocode from "react-geocode";
+import axios from 'axios'
+import App from './Map-Vehicles-React/src/index'
 
 export default class MapDelivery extends Component{
 
 	render(){
 		return(
-				<MapContainer/>
+				<App/>
 	
 			)
 	}
