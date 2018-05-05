3510639bd112757c0941f5c3bd55dc3e66350d39
lihaojin <hli002@citymail.cuny.edu>
2018-05-04 23:48:08 -0400

    fixed request

--- src/Utils/Requests/OrderRequest.js
+++ src/Utils/Requests/OrderRequest.js
@@ -3,6 +3,9 @@ import axios from 'axios';
 const baseUrl = "http://localhost:3001";
 
 export {OrderRequest}
-  function OrderRequest(){
-    return axios.post(baseUrl + 'makeVisitorOrder')
+  function OrderRequest(items){
+    return axios.post(baseUrl + 'makeVisitorOrder',{
+      items: items
+    }
+  )
   }
