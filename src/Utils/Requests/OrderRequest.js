e352cec868b7912485c6357f924607325e6d29a2
lihaojin <hli002@citymail.cuny.edu>
2018-05-09 16:21:26 -0400

    customer request added

--- src/Utils/Requests/OrderRequest.js
+++ src/Utils/Requests/OrderRequest.js
@@ -3,11 +3,23 @@ import axios from 'axios';
 const baseUrl = "http://localhost:3001";
 
 export {OrderRequest}
-  function OrderRequest(name,items,destination){
+  function OrderRequest(name,items,destination,phone_number){
     return axios.post(baseUrl + '/store/makeVisitorOrder',{
       name: name,
       items: items,
-      destination: destination
+      destination: destination,
+      phone_number: phone_number
     }
   )
   }
+
+  export {customerOrder}
+    function customerOrder(name,items,destination,phone_number){
+      return axios.post(baseUrl + '/customer/makeOrder',{
+        name: name,
+        items: items,
+        destination: destination,
+        phone_number: phone_number
+      }
+    )
+    }
